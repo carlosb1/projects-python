c3ee4ff87e330701e81736d6063d413452eb76eb
Carlos B <carlos.baezruiz@gmail.com>
2019-04-17 20:24:19 +0200

    Updated files to set up environment

--- flask-news/run.sh
+++ flask-news/run.sh
@@ -2,6 +2,6 @@ export FLASK_RUN_PORT=5002
 export FLASK_APP=run.py
 #flask run & >> backend.log
 gunicorn app:app -b 0.0.0.0:5002 & >> backend.log
-celery -A tasks worker --loglevel=info
+# celery -A tasks worker --loglevel=info
 cd frontend && npm run dev & >> frontend.log
 
