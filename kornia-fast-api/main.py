1ace0bcbff80938be92565703c73487d740cf77b
carlosb1 <mcflurry0@gmail.com>
2022-01-07 19:32:35 +0100

    Added spaguetti code.

--- kornia-fast-api/main.py
+++ kornia-fast-api/main.py
@@ -1,38 +1,50 @@
+from fastapi import WebSocket
 from typing import Optional
 from fastapi import FastAPI, File, UploadFile
+from fastapi.staticfiles import StaticFiles
 from abc import ABC
-
-import torch
+#import numpy as np
+#import cv2
 import kornia
+import base64
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+# def image_to_base64(img: np.ndarray) -> bytes:
+#     """ Given a numpy 2D array, returns a JPEG image in base64 format """
+#
+#     # using opencv 2, there are others ways
+#     img_buffer = cv2.imencode('.jpg', img)[1]
+#     return base64.b64encode(img_buffer).decode('utf-8')
+#
+#
+# def get_image(volume, index: int):
+#     image = volume[:, :, index]
+#     return image_to_base64(image)
+#
+
+# @app.websocket("/ws"
+# async def websocket_endpoint(websocket: WebSocket):
+#    print("started")
+#    await websocket.accept()
+#    try:
+#        while True:
+#            data = await websocket.receive_text()
+#            print(f"received: {int(data)}")
+#            index = int(data)
+#            image = get_image(volume, index)
+#            await websocket.send_bytes(image)
+#    except Exception as e:
+#        print(e)
+#    finally:
+#        websocket.close()
+#
 
 
-class Command(ABC):
-    def name(self):
-        pass
-
-
-class CommandHandler(ABC):
-    def execute(self, command):
-        pass
-
-
-class KorniaController:
-    def run(self):
-        pass
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.post("/files/")
-async def create_file(file: bytes = File(...)):
-    return {"file_size": len(file)}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
